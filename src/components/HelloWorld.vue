1150eeb91a750fd8a025e37fe40a8ff1c53c8284
A_ito <BoxPistols@users.noreply.github.com>
2018-11-05 01:56:57 +0900

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2 v-show="showText">{{ tex }}</h2>  
+  <div class="hello">    
+    <h1>Are you like this?</h1>    
+    
+    <div class="org_likeArea" v-on:click="toggle" v-show="tgl">
+      <h2 v-show="showMsg">{{ msg }}</h2>
+      <h2 v-show="showText">{{ tex }}</h2>
+
+      <transition>
+      <div class="org_mark" v-bind:class='{active:isActive}' v-if="this.showText">
+        <p class="m_like"><span class="a_mark add">♡</span></p>
+        <p class="a_count">{{ count + 1 }}</p>
+      </div>
+      <div class="org_mark" v-else>
+        <p class="m_like"><span class="a_mark">♡</span></p>
+        <p class="a_count">{{ count }}</p>
+      </div>
+      </transition>  
+    </div>        
+  
   </div>
 </template>
 
@@ -9,16 +25,28 @@
 export default {
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      tex: 'Tex Data',
+      tgl: 'toggle',
+      msg: 'Please like!',
+      tex: 'Remove like...',
+      isActive: true,
+      showMsg: true,
       showText: false,
+      count: 1,
     };
+  },
+  methods: {
+    toggle: function() {
+      this.showText = !this.showText;
+      this.showMsg = !this.showMsg;
+      //       this.count = this.count;
+      //       count = count + 1;
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang='scss' scoped>
 h1,
 h2 {
   font-weight: normal;
@@ -33,5 +61,59 @@ li {
 }
 a {
   color: #42b983;
+}
+
+button.btn {
+  padding: 15px 20px;
+}
+.active .add {
+  color: red;
+}
+
+.org_likeArea {
+  //   background: #eee;
+  cursor: pointer;
+  width: fit-content;
+  margin: auto;
+}
+
+.org_mark {
+  //   background: aliceblue;
+  //   display: flex;
+  display: inline-flex;
+  align-items: center;
+}
+.m_like {
+  //   transition: 0.3s;
+  position: relative;
+  //   min-width: 100px;
+  .a_mark {
+    font-size: 20px;
+    &.add {
+      animation-name: sizeScale;
+      animation-duration: 0.3s;
+    }
+  }
+  .a_count {
+    position: absolute;
+    right: 0;
+    top: 50%;
+  }
+}
+@keyframes sizeScale {
+  0% {
+    font-size: 20px;
+  }
+  20% {
+    font-size: 28px;
+  }
+
+  50% {
+    font-size: 24px;
+  }
+
+  100% {
+    font-size: 20px;
+  }
 }
 </style>
